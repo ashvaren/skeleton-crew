aa81f92cf532bafaf4185d6369905195df0c0fc4
ashvaren <mike@ashvaren.com>
2026-09-25 21:09:15 +0100

    Skeleton Crew v0.1: service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const C='skeleton-crew-v1';const FILES=['./','./index.html','./manifest.json','./icon.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{const u=new URL(e.request.url);if(u.origin!==location.origin){e.respondWith(fetch(e.request).catch(()=>new Response('',{status:503})));return;}
+e.respondWith(fetch(e.request).then(r=>{const cp=r.clone();caches.open(C).then(c=>c.put(e.request,cp));return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));});
